812b22acc3a1fc55c1b7f73e46d889e0fd73882e
danilovalerio <danilo_vs@hotmail.com>
2018-10-30 16:10:17 -0300

    Método que lista formas, agora aceita qualquer subtipo da interface Forma

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -15,12 +15,18 @@ public class Programa {
 		List<Forma> minhasFormas = new ArrayList<>();
 		minhasFormas.add(new Retangulo(3.0,2.0));
 		minhasFormas.add(new Circulo(2.0));
-			
-		System.out.println("Área total: "+totalArea(minhasFormas));
+	
+		List<Circulo> meusCirculos = new ArrayList<>();
+		meusCirculos.add(new Circulo(1.5));
+		meusCirculos.add(new Circulo(2.5));
+		meusCirculos.add(new Circulo(3.0));
 		
+		System.out.println("Área total: "+totalArea(minhasFormas));
+		System.out.println("Área total: "+totalArea(meusCirculos));
 	}
 	
-	public static double totalArea(List<Forma> list) {
+	//Irá aceitar qualquer lista que seja um tipo de Forma
+	public static double totalArea(List<? extends Forma> list) {
 		double soma = 0.0;
 		for(Forma s : list) {
 			soma += s.area();
